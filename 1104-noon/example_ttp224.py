b0adfe55a5d20ba7095b379376b53e50bfb90aee
chu3an <chuan9129@gmail.com>
2020-11-03 17:23:15 +0800

    New hcsr01 example

--- 1104-noon/example_ttp224.py
+++ 1104-noon/example_ttp224.py
@@ -3,13 +3,9 @@ import time
 from RPi import GPIO
 
 if __name__ == "__main__":
-
     GPIO.setmode(GPIO.BCM)
     PAD_Pin = 17
-    LED_Pin = 27
     GPIO.setup(PAD_Pin, GPIO.IN)
-    GPIO.setup(LED_Pin, GPIO.OUT)
-    light = False
     try:
         a = 0
         while True:
